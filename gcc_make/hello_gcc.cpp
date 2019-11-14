5f3d1f35332d1dacfd7f9d3c07e632d57e6de0a6
SCUPRISCILLA <941705864@qq.com>
2019-11-14 23:10:36 +0800

    G++ and makefile to make an application

---
+++ gcc_make/hello_gcc.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello world! I'm complied by gcc and makefile" << endl;
+	cout << "add new line" << endl;
+	cout << "add new line again" << endl;
+	return 0;
+}
