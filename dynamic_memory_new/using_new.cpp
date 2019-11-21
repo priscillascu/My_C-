400d06eff4e485ebee1e2b611604da369ea326a5
SCUPRISCILLA <941705864@qq.com>
2019-11-21 13:08:39 +0800

    new的用途，分配数组和二次数组

--- dynamic_memory_new/using_new.cpp
+++ dynamic_memory_new/using_new.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void make2dArray(int * &x, int rows_num, int cols_num);
-
-int *y;  //声明一个整数指针变量
-int *a;
-int *x;
-
 int main(int argc, char const *argv[])
 {
+	int *y = nullptr;  //声明一个整数指针变量
+	int *a = nullptr;
+	int **x = nullptr;
+	int cols_num = 3;
+	int rows_num = 3;
+
 	y = new int(10);   //使用new分配一个新的能够存储一个整数的空间，并赋值10
 	cout << "空间赋值后为：" << *y << endl;
 
@@ -23,22 +23,26 @@ int main(int argc, char const *argv[])
 	delete []a;   //释放空间，数组要把[]提前
 	cout << "空间释放后y为: " << *y << endl;   //释放后y=0
 
-	make2dArray(x, 3, 3);
-	for(int i = 0; i < 3; i++)
-	{
-		for(int j = 0; j < 3; j++)
-		{
-			cout << x[i][j];
-		}
-	}
-	return 0;
-}
-
-void make2dArray(int * &x, int rows_num, int cols_num)
-{
-	x = new int(rows_num);
+	x = new int *[cols_num];   //使用new定义二维数组
 	for(int i = 0; i < rows_num; i++)
 	{
-		x[i] = new int(rows_num);
+		x[i] = new int[cols_num];
 	}
+	cout << x[2][2] << endl;
+
+	for(int i = 0; i < rows_num; i++)
+	{
+		for(int j = 0; j < cols_num; j++)
+		{
+			x[i][j] = 1;
+		}
+	}
+	cout << x[2][2] << endl;
+
+	for(int i = 0; i < rows_num; i++)   //释放二维数组空间
+	{
+		delete []x[i];
+	}
+	delete []x;
+	return 0;
 }
