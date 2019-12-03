f0e603066280eb730b142242f023c184e6f98b95
SCUPRISCILLA <941705864@qq.com>
2019-12-03 10:27:12 +0800

    二叉树建立和前序遍历、中序遍历、后序遍历

--- tree_binary/main.cpp
+++ tree_binary/main.cpp
@@ -5,15 +5,11 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	BiT_node *T1;
-	BiT_node *T2;
 	T1 = new BiT_node;
-	T1->data = 1;
-	//T1->lchild = nullptr;
-	//T1->rchild = nullptr;
-	T2 = T1->lchild;
-	T2 = new BiT_node;
-	T2->data = 2;
-	
+	CreatBinaryTree(T1);
+	cout << "前序遍历为：" << endl;
 	PreOrderTraverse(T1);
+	cout << "中序遍历为：" << endl;
+	InOrderTraverse(T1);
 	return 0;
 }
--- tree_binary/tree_binary.hpp
+++ tree_binary/tree_binary.hpp
@@ -10,6 +10,9 @@ struct BiT_node
 };
 
 void PreOrderTraverse(BiT_node *T1);  //前序遍历
+void InOrderTraverse(BiT_node *T1);  //中序遍历
+void PostOrderTraverse(BiT_node *T1);  //后序遍历
+void CreatBinaryTree(BiT_node *T1);
 
 void PreOrderTraverse(BiT_node *T1)  //前序遍历
 {
@@ -20,9 +23,60 @@ void PreOrderTraverse(BiT_node *T1)  //前序遍历
 	}
 	else
 	{
-		cout << "元素为: " << T1->data;
+		cout << "元素为: " << T1->data << endl;
 		PreOrderTraverse(T1->lchild);
 		PreOrderTraverse(T1->rchild);
 		//return;
+	}
+}
+
+void InOrderTraverse(BiT_node *T1)  //中序遍历
+{
+	if(T1 == nullptr)
+	{
+		cout << "无孩子结点" << endl;
+		//return;
+	}
+	else
+	{
+		InOrderTraverse(T1->lchild);
+		cout << "元素为: " << T1->data << endl;
+		InOrderTraverse(T1->rchild);
+		//return;
+	}
+}
+
+void PostOrderTraverse(BiT_node *T1)  //后序遍历
+{
+	if(T1 == nullptr)
+	{
+		cout << "无孩子结点" << endl;
+		//return;
+	}
+	else
+	{
+		PostOrderTraverse(T1->lchild);
+		PostOrderTraverse(T1->rchild);
+		cout << "元素为: " << T1->data << endl;
+		//return;
+	}
+}
+
+void CreatBinaryTree(BiT_node *T1)  //创建二叉树
+{
+	int ch;
+	cout << "请输入二叉树前序遍历字符数据，空用0替代：" << endl;
+	cin >> ch;
+	if(ch == 0)
+	{
+		T1 = nullptr;
+	}
+	else
+	{
+		T1->lchild = new BiT_node;
+		T1->rchild = new BiT_node;
+		T1->data = ch;
+		CreatBinaryTree(T1->lchild);
+		CreatBinaryTree(T1->rchild);
 	}
 }
